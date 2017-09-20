90058a9ef69c6b1e0dba2c725e98c18dbf7b811e
vinnj <vinn.joseph@gmail.com>
2017-09-21 00:23:49 +0100

    Create functionality added

--- UpdateCRM/NoteAttachment/AgreementNoteAttachment.cs
+++ UpdateCRM/NoteAttachment/AgreementNoteAttachment.cs
@@ -1,30 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Tooling;
-using Microsoft.Xrm.Tooling.Connector;
 using System.Configuration;
 using System.IO;
+using System.Linq;
+using Debeers.Xrm.Entities;
 using LinqToExcel;
 using LinqToExcel.Attributes;
-using Debeers.Xrm.Entities;
+using LinqToExcel.Domain;
+using LinqToExcel.Query;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
 
 namespace NoteAttachment
 {
-    class AgreementNoteAttachment
+    internal class AgreementNoteAttachment
     {
-        [ExcelColumn("RecordName")]
-        public string RecordName { get; set; }
-
-        [ExcelColumn("FileName")]
-        public string FileName { get; set; }
-        
-        [ExcelColumn("Account")]
-        public string Account { get; set; }
-
         //Change these values as required
 
         //KYC - C:\FilesToUpload\KYC\KYC.xlsx
@@ -37,23 +26,30 @@ namespace NoteAttachment
 
         //EntityReference entityRef = new EntityReference();
 
-        string fileLocation = @"C:\FilesToUpload\Agreement\Agreement.xlsx";
-        string folderLocation = @"C:\FilesToUpload\Agreement\";
-        string subject = "Agreement";
+        private readonly string fileLocation = @"C:\FilesToUpload\Agreement\Agreement.xlsx";
+        private readonly string folderLocation = @"C:\FilesToUpload\Agreement\";
+        private readonly string subject = "Agreement";
+
+        [ExcelColumn("RecordName")]
+        public string RecordName { get; set; }
+
+        [ExcelColumn("FileName")]
+        public string FileName { get; set; }
+
+        [ExcelColumn("Account")]
+        public string Account { get; set; }
 
         private ExcelQueryFactory ReadData()
         {
-
             var fileUploadValues = new ExcelQueryFactory(fileLocation)
             {
-                DatabaseEngine = LinqToExcel.Domain.DatabaseEngine.Ace,
-                TrimSpaces = LinqToExcel.Query.TrimSpacesType.Both,
+                DatabaseEngine = DatabaseEngine.Ace,
+                TrimSpaces = TrimSpacesType.Both,
                 UsePersistentConnection = true,
                 ReadOnly = true
             };
 
             return fileUploadValues;
-
         }
 
         public void PassValues()
@@ -61,71 +57,66 @@ namespace NoteAttachment
             var fileValueExcel = ReadData();
 
             var files = from p in fileValueExcel.Worksheet<AgreementNoteAttachment>("Values")
-                        select p;
+                select p;
 
             foreach (var file in files)
             {
-
-                string _recordName = file.RecordName;
-                string _fileName = file.FileName;
-                EntityReference _accountId = new EntityReference("account", new Guid(file.Account));
+                var _recordName = file.RecordName;
+                var _fileName = file.FileName;
+                var _accountId = new EntityReference("account", new Guid(file.Account));
 
                 //Annotation(_fileName);
                 GetRecordID(_recordName, _fileName, _accountId);
-
             }
         }
 
         private void GetRecordID(string recordName, string fileName, EntityReference accountID)
         {
+            var connStr = ConfigurationManager.ConnectionStrings[1].ConnectionString;
 
-            string connStr = ConfigurationManager.ConnectionStrings[1].ConnectionString;
-
-            CrmServiceClient conn = new CrmServiceClient(connStr);
+            var conn = new CrmServiceClient(connStr);
 
             // Cast the proxy client to the IOrganizationService interface.
             //_orgService = (IOrganizationService)conn.OrganizationWebProxyClient != null ? (IOrganizationService)conn.OrganizationWebProxyClient : (IOrganizationService)conn.OrganizationServiceProxy;
-            IOrganizationService service = (IOrganizationService)conn.OrganizationServiceProxy;
+            IOrganizationService service = conn.OrganizationServiceProxy;
 
             //Linq to retrieve Record Guid
-            OrgServiceContext svc = new OrgServiceContext(service);
+            var svc = new OrgServiceContext(service);
 
             var recordGuid = from r in svc.deb_agreementSet
-                             where r.deb_name == recordName &&
-                             r.deb_AccountId == accountID
-                             select r;
+                where r.deb_name == recordName &&
+                      r.deb_AccountId == accountID
+                select r;
 
             foreach (var r in recordGuid)
             {
-                string _entity = r.LogicalName;
-                string Id = r.Id.ToString();
-                string _accountName = r.deb_AccountId.Name;
+                var _entity = r.LogicalName;
+                var Id = r.Id.ToString();
+                var _accountName = r.deb_AccountId.Name;
 
                 Annotation(Id, fileName, _entity, recordName, _accountName);
             }
-
-
         }
 
         private void Annotation(string recordId, string fileName, string entity, string recordName, string accountName)
         {
-            if(fileName != null)
+            if (fileName != null)
             {
-                string connStr = ConfigurationManager.ConnectionStrings[1].ConnectionString;
+                var connStr = ConfigurationManager.ConnectionStrings[1].ConnectionString;
 
-                CrmServiceClient conn = new CrmServiceClient(connStr);
+                var conn = new CrmServiceClient(connStr);
 
-                IOrganizationService service = (IOrganizationService)conn.OrganizationServiceProxy;
+                IOrganizationService service = conn.OrganizationServiceProxy;
 
 
-                FileStream _stream = File.OpenRead(folderLocation + fileName);
-                byte[] _bData = new byte[_stream.Length];
+                var _stream = File.OpenRead(folderLocation + fileName);
+                var _bData = new byte[_stream.Length];
                 _stream.Read(_bData, 0, _bData.Length);
                 _stream.Close();
-                string encodedData = System.Convert.ToBase64String(_bData);
+                var encodedData = Convert.ToBase64String(_bData);
 
 
-                Entity _annotation = new Entity("annotation");
+                var _annotation = new Entity("annotation");
                 _annotation.Attributes["objectid"] = new EntityReference(entity, new Guid(recordId));
                 _annotation.Attributes["objecttypecode"] = entity;
                 _annotation.Attributes["subject"] = subject;
@@ -138,9 +129,6 @@ namespace NoteAttachment
 
                 Console.WriteLine("File attached to " + accountName);
             }
-           
         }
-
     }
 }
-
